5bfe54bcc33c74e56ac3a8ead20e2af0921ba455
Gustavo-Maximo <gmaximodasilva@gmail.com>
2025-01-23 19:42:49 -0300

    Criação da classe Program

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+public class Program {
+  public static void main(String args[]) {
+   
+        Pessoa pessoa1 = new Pessoa("Soraya", 18, "Feminino", 1.65f, 60.0f);
+        Pessoa pessoa2 = new Pessoa("Gustavo", 19, "Masculino", 1.80f, 60.0f);
+        Pessoa pessoa3 = new Pessoa("Mariana", 20, "Feminino", 1.55f, 48.0f);
+
+        
+        pessoa1.mostrarDados();
+        pessoa2.mostrarDados();
+        pessoa3.mostrarDados();
+   
+   
+   
+   
+   
+  }
+}
